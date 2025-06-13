fde54ba6b5e24cc9f6ff64b0807aa0d3afdb7270
NOAROFGOD <94696999+NOAROFGOD@users.noreply.github.com>
2025-06-14 05:23:06 +0700

    restore.bat

--- restore.bat
+++ restore.bat
@@ -66,3 +66,41 @@ reg delete "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v KeepAliv
 
 :: ล้าง DNS cache
 ipconfig /flushdns
+
+set KEY="HKLM\SYSTEM\ControlSet001\Services\Tcpip\Parameters"
+
+reg delete %KEY% /v DataBasePath /f
+reg delete %KEY% /v Domain /f
+reg delete %KEY% /v ICSDomain /f
+reg delete %KEY% /v IPEnableRouter /f
+reg delete %KEY% /v HostName /f
+reg delete %KEY% /v NV HostName /f
+reg delete %KEY% /v TcpAckFrequency /f
+reg delete %KEY% /v TcpDelAckTicks /f
+reg delete %KEY% /v TCPNoDelay /f
+reg delete %KEY% /v TcpWindowSize /f
+reg delete %KEY% /v SackOpts /f
+reg delete %KEY% /v TcpMaxDataRetransmissions /f
+reg delete %KEY% /v TCPTimedWaitDelay /f
+reg delete %KEY% /v DefaultTTL /f
+reg delete %KEY% /v KeepAliveTime /f
+reg delete %KEY% /v KeepAliveInterval /f
+reg delete %KEY% /v TcpMaxDupAcks /f
+reg delete %KEY% /v EnablePMTUBHDetect /f
+reg delete %KEY% /v EnablePMTUDiscovery /f
+reg delete %KEY% /v GlobalMaxTcpWindowSize /f
+reg delete %KEY% /v DisableTaskOffload /f
+reg delete %KEY% /v SynAttackProtect /f
+reg delete %KEY% /v MaxUserPort /f
+reg delete %KEY% /v DeadGWDetectDefault /f
+reg delete %KEY% /v DhcpNameServer /f
+reg delete %KEY% /v Tcp1323 /f
+
+:: ลบค่าใน Interfaces เฉพาะอันที่ระบุ
+set IFKEY="HKLM\SYSTEM\ControlSet001\Services\Tcpip\Parameters\Interfaces\{b58adb3b-0c69-4ea3-8105-df8832c12608}"
+
+reg delete %IFKEY% /v EnableDHCP /f
+reg delete %IFKEY% /v DhcpIPAddress /f
+reg delete %IFKEY% /v DhcpSubnetMask /f
+reg delete %IFKEY% /v DhcpServer /f
+reg delete %IFKEY% /v DhcpNameServer /f
