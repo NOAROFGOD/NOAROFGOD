0501c2b925079d4948d92fe0c0e793ffe2b277ea
NOAROFGOD <94696999+NOAROFGOD@users.noreply.github.com>
2026-04-03 03:50:21 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  TAX: 0.065,
+  MIN_PROFIT: 5000,
+  TOP_N: 20,
+  TIERS: [4,5,6,7,8],
+  ENCHANTS: [0,1,2,3,4],
+  CITIES: [
+    "Bridgewatch",
+    "Martlock",
+    "Thetford",
+    "Lymhurst",
+    "Fort Sterling",
+    "Caerleon"
+  ],
+  CHUNK_SIZE: 100
+};
