ef56c5cbb718d1649396f6bbc512a1a2ffc1733e
NOAROFGOD <94696999+NOAROFGOD@users.noreply.github.com>
2026-04-03 05:28:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 module.exports = {
   TAX: 0.065,
-  MIN_PROFIT: 1000,
+
+  // 🔥 ปรับใหม่
+  MIN_PROFIT: 300,
+  MIN_PERCENT: 5,
+
   TOP_N: 20,
+
   TIERS: [4, 5, 6, 7, 8],
   ENCHANTS: [0, 1, 2, 3, 4],
+
   CITIES: [
     "Bridgewatch",
     "Martlock",
@@ -13,9 +19,9 @@ module.exports = {
     "Caerleon"
   ],
 
-  // 🔥 แก้ตรงนี้
-  CHUNK_SIZE: 50,      
-  REQUEST_DELAY: 300,   
-  RETRY_DELAY: 2000,   
+  CHUNK_SIZE: 50,
+  REQUEST_DELAY: 300,
+  RETRY_DELAY: 2000,
+
   CACHE_TTL: 120
 };
